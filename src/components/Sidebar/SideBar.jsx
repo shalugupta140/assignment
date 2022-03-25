bc0f9d441ea79d48d37716986ffbf27dd6d29616
shalu gupta <shalu14013045@gmail.com>
2022-03-25 13:44:14 +0530

    code refector

--- src/components/Sidebar/SideBar.jsx
+++ src/components/Sidebar/SideBar.jsx
@@ -1,124 +1,55 @@
 import { NavLink } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { AiFillQuestionCircle } from "react-icons/ai";
-import { BsFilesAlt, BsFillGridFill } from "react-icons/bs";
+import {  BsFillGridFill } from "react-icons/bs";
+import { BiCollection } from "react-icons/bi";
 import { FiEdit } from "react-icons/fi";
 import { IoLogOut } from "react-icons/io5";
-import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 import SidebarMenu from "./SidebarMenu";
 const routes = [
   {
     path: "/",
-    name: "Home",
     icon: <FaHome size={22}/>,
   },
   {
     path: "/users",
-    name: "Users",
     icon: <BsFillGridFill size={22}/>,
   },
   {
     path: "/folders",
-    name: "Folders",
-    icon: <BsFilesAlt size={22}/>,
+    icon: <BiCollection size={22}/>,
   },
   {
     path: "/faq",
-    name: "FAQ",
     icon: <AiFillQuestionCircle size={22}/>,
   },
   {
     path: "/edit",
-    name: "Edit",
     icon: <FiEdit size={22} />,
   },
-
   {
     path: "/logout",
-    name: "logout",
     icon: <IoLogOut size={22}/>,
   },
- 
 ];
 
-
-
 const SideBar = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
-  const inputAnimation = {
-    hidden: {
-      width: 0,
-      padding: 0,
-      transition: {
-        duration: 0.2,
-      },
-    },
-    show: {
-      width: "140px",
-      padding: "5px 15px",
-      transition: {
-        duration: 0.2,
-      },
-    },
-  };
-
-  const showAnimation = {
-    hidden: {
-      width: 0,
-      opacity: 0,
-      transition: {
-        duration: 0.5,
-      },
-    },
-    show: {
-      opacity: 1,
-      width: "auto",
-      transition: {
-        duration: 0.5,
-      },
-    },
-  };
-
   return (
     <>
       <div className="main-container">
-        <motion.div
-          animate={{
-            width: isOpen ? "200px" : "75px",
-            transition: {
-              duration: 0.5,
-              type: "spring",
-              damping: 10,
-            },
-          }}
+        <div
+          style={{
+            width: "75px"
+            }}
           className={`sidebar`}
         >
-          <div className="top_section">
-            <AnimatePresence>
-              {isOpen && (
-                <motion.h1
-                  variants={showAnimation}
-                  initial="hidden"
-                  animate="show"
-                  exit="hidden"
-                  className="logo"
-                >
-                </motion.h1>
-              )}
-            </AnimatePresence>
-
-          </div>
+          <div className="top_section"></div>
           <section className="routes">
             {routes.map((route, index) => {
               if (route.subRoutes) {
                 return (
                   <SidebarMenu
-                    setIsOpen={setIsOpen}
                     route={route}
-                    showAnimation={showAnimation}
-                    isOpen={isOpen}
                   />
                 );
               }
@@ -127,27 +58,13 @@ const SideBar = ({ children }) => {
                   to={route.path}
                   key={index}
                   className="link"
-                  activeClassName="active"
-                >
+                  activeClassName="active">
                   <div className="icon">{route.icon}</div>
-                  <AnimatePresence>
-                    {isOpen && (
-                      <motion.div
-                        variants={showAnimation}
-                        initial="hidden"
-                        animate="show"
-                        exit="hidden"
-                        className="link_text"
-                      >
-                        {route.name}
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
                 </NavLink>
               );
             })}
           </section>
-        </motion.div>
+        </div>
         <main>{children}</main>
       </div>
     </>
